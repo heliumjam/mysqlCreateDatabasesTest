2b8637558d3e152bd80f7cdebdb4e83f4eaaf903
heliumjam <little.ej3@gmail.com>
2023-11-03 19:16:28 +0200

    Initial commit

---
+++ src/main/java/org/example/CreateDatabasesAndTables.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class CreateDatabasesAndTables {
+    //Datbase connection
+    String url ="jdbc:mysql://127.17.0.2:336/";
+    String username = "root";
+    String password = "12345678";
+    String databaseName = "testIdea";
+
+
+}
